d1b280ad9bdb8335402e71e0110a55aa4f92d00f
xfcdxg <xfcdxg_zq@163.com>
2026-03-29 23:15:32 +0800

    update

--- src/config/pages/Exclusive.config.js
+++ src/config/pages/Exclusive.config.js
@@ -2,7 +2,7 @@ export default {
   hero2: {
     title: { en: "Exclusive Listings", zh: "独家房源" },
     description: {
-      en: "Emma Ju represents sellers across Manhattan, Bergen County, and Hudson County,<br/> showcasing exclusive listings in sought-after communities such as Fort Lee, Edgewater, Englewood Cliffs, Weehawken, and Jersey City.",
+      en: "Emma Ju represents sellers across Manhattan, Bergen County, and Hudson County, showcasing exclusive listings in sought-after communities such as Fort Lee, Edgewater, Englewood Cliffs, Weehawken, and Jersey City.",
       zh: "Emma Ju 覆盖曼哈顿、Bergen 郡与 Hudson 郡，展示 Fort Lee、Edgewater、Englewood Cliffs、Weehawken、Jersey City 等热门社区的独家房源。",
     },
   },
