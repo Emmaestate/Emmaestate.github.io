f81a44b7b4e7452f769ae82f51401b96887d74bb
xfcdxg <xfcdxg_zq@163.com>
2026-05-15 20:42:44 +0800

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,6 +7,7 @@ import youtube_icon from "../../assets/youtube.png";
 import facebook_icon from "../../assets/facebook.png";
 import companyLogo from "../../assets/company_b.png";
 import footer_logo from "../../assets/footer-logo_b.png";
+import realtorLogo from "../../assets/realtor.png";
 import { useLanguage } from "../../i18n/LanguageContext.jsx";
 import footerConfig from "../../config/pages/Footer.config.js";
 
@@ -23,8 +24,10 @@ const Footer = () => {
         <div className="footer-middle-row">
           {/* Column 1: Specialization & Logos & Socials */}
           <div className="footer-col footer-col-left">
-            <h4 className="footer-col-title-left">{footerConfig.left.specialization[lang]}</h4>
-            
+            <h4 className="footer-col-title-left">
+              {footerConfig.left.specialization[lang]}
+            </h4>
+
             <div className="footer-logos">
               <a
                 href="#"
@@ -54,6 +57,12 @@ const Footer = () => {
                   className="footer-company-logo"
                 />
               </a>
+              <span className="logo-divider"></span>
+              <img
+                src={realtorLogo}
+                alt="Realtor"
+                className="footer-company-logo"
+              />
             </div>
 
             <div className="container-social-media">
@@ -99,22 +108,33 @@ const Footer = () => {
 
           {/* Column 2: Location */}
           <div className="footer-col footer-col-location">
-            <h4 className="footer-col-title">{footerConfig.left.locationTitle[lang]}</h4>
+            <h4 className="footer-col-title">
+              {footerConfig.left.locationTitle[lang]}
+            </h4>
             <div className="footer-location-item">
               <span className="location-dot">• </span>
               <div className="location-text">
-                <span className="location-label">{footerConfig.left.njLabel[lang]}</span>
+                <span className="location-label">
+                  {footerConfig.left.njLabel[lang]}
+                </span>
                 <p>
-                  {footerConfig.left.njAddr1[lang]} {footerConfig.left.njAddr2[lang]}
+                  {footerConfig.left.njAddr1[lang]}{" "}
+                  {footerConfig.left.njAddr2[lang]}
                 </p>
               </div>
             </div>
-            <div className="footer-location-item" style={{ marginTop: '1.5rem' }}>
+            <div
+              className="footer-location-item"
+              style={{ marginTop: "1.5rem" }}
+            >
               <span className="location-dot">• </span>
               <div className="location-text">
-                <span className="location-label">{footerConfig.left.nyLabel[lang]}</span>
+                <span className="location-label">
+                  {footerConfig.left.nyLabel[lang]}
+                </span>
                 <p>
-                  {footerConfig.left.nyAddr1[lang]}, {footerConfig.left.nyAddr2[lang]}
+                  {footerConfig.left.nyAddr1[lang]},{" "}
+                  {footerConfig.left.nyAddr2[lang]}
                 </p>
               </div>
             </div>
@@ -122,7 +142,9 @@ const Footer = () => {
 
           {/* Column 3: Contact Info */}
           <div className="footer-col footer-col-contact">
-            <h4 className="footer-col-title">{footerConfig.contactTitle[lang]}</h4>
+            <h4 className="footer-col-title">
+              {footerConfig.contactTitle[lang]}
+            </h4>
             <div className="footer-contact-item">
               <span className="contact-icon">✉️</span>
               <a href="mailto:emma@acrenj.us" className="footer-contact-link">
@@ -141,7 +163,9 @@ const Footer = () => {
           <div className="footer-col footer-col-message">
             <h4 className="footer-col-title">{footerConfig.title[lang]}</h4>
             <p className="footer-cta-desc">
-              Interested in buying or selling?<br/>Let's connect and find your perfect solution.
+              Interested in buying or selling?
+              <br />
+              Let's connect and find your perfect solution.
             </p>
             <button
               className="footer-cta-btn"
@@ -154,8 +178,13 @@ const Footer = () => {
 
         {/* Bottom Full-Width Row: Disclaimer & Copyright */}
         <div className="footer-bottom-info">
-          <span className="footer-disclaimer">All information is deemed reliable but not guaranteed and should be independently reviewed and verified.</span>
-          <span className="footer-copyright">Copyright © 2026 Emma Ju Estates - All Rights Reserved.</span>
+          <span className="footer-disclaimer">
+            All information is deemed reliable but not guaranteed and should be
+            independently reviewed and verified.
+          </span>
+          <span className="footer-copyright">
+            Copyright © 2026 Emma Ju Estates - All Rights Reserved.
+          </span>
         </div>
       </div>
     </div>
